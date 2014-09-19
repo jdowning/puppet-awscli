0c6bb182006bf01ecd4564aafd3991d64fab94fa
Geoffrey Hoffman <geoffrey.hoffman@gmail.com>
2014-09-19 10:23:11 -0700

    Guessing this was just a bad paste.
    Changed from wal_e to awscli

--- manifests/deps.pp
+++ manifests/deps.pp
@@ -18,6 +18,6 @@ class awscli::deps {
     'RedHat': {
       include awscli::deps::redhat
     }
-    default:  { fail('The wal_e module currently only suports Debian and RedHat families') }
+    default:  { fail('The awscli module currently only suports Debian and RedHat families') }
   }
 }

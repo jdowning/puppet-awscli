02dc1b4221ab60857c37d2e79f29c9b9de00d2b7
Luis Osses Lopez <llopez@sugarcrm.com>
2015-06-02 19:41:24 +0200

    Added the possibility to specify the package names
    This will update the compatibility to be used in Debian 8.0 (or possibly others) and using specific versions of the Python packages

--- manifests/deps/debian.pp
+++ manifests/deps/debian.pp
@@ -3,10 +3,10 @@
 # This module manages awscli dependencies for Debian $::osfamily.
 #
 class awscli::deps::debian {
-  if ! defined(Package['python-dev']) {
-    package { 'python-dev': ensure => installed }
+  if ! defined(Package[ $awscli::pkg_dev ]) {
+    package { $awscli::pkg_dev: ensure => installed }
   }
-  if ! defined(Package['python-pip']) {
-    package { 'python-pip': ensure => installed }
+  if ! defined(Package[ $awscli::pkg_pip ]) {
+    package { $awscli::pkg_pip: ensure => installed }
   }
 }

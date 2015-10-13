4591b0cc80b169a9eb71ff8db820fcbf2df6daf3
Todd Courtnage <todd@chaordix.com>
2015-10-13 12:57:01 -0600

    Updates

--- manifests/deps/debian.pp
+++ manifests/deps/debian.pp
@@ -3,24 +3,17 @@
 # This module manages awscli dependencies for Debian $::osfamily.
 #
 class awscli::deps::debian {
-#  constraint {
-#    "python-dev-package":
-#      resource  => Package['python-dev'],
-#      allow     => { ensure => ['installed', 'present', 'absent' ] },
-#      weak      => true;
-#  }
-
-#  constraint {
-#    "python-pip-package":
-#      resource  => Package['python-pip'],
-#      allow     => { ensure => ['installed', 'present', 'absent' ] },
-#      weak      => true;
-#  }
-
-  if ! defined(Package['python-dev']) {
-    package { 'python-dev': ensure => installed }
+  constraint {
+    "python-dev-package":
+      resource  => Package['python-dev'],
+      allow     => { ensure => [ 'present' ] },
+      weak      => true;
   }
-  if ! defined(Package['python-pip']) {
-    package { 'python-pip': ensure => installed }
+
+  constraint {
+    "python-pip-package":
+      resource  => Package['python-pip'],
+      allow     => { ensure => [ 'present' ] },
+      weak      => true;
   }
 }

a6ced5097b280afd13e495aa15ba5637b8f70dff
Todd Courtnage <todd@chaordix.com>
2015-10-13 13:12:38 -0600

    Bring weakness back.

--- manifests/deps/debian.pp
+++ manifests/deps/debian.pp
@@ -6,12 +6,14 @@ class awscli::deps::debian {
   constraint {
     "python-dev-package":
       resource  => Package['python-dev'],
-      allow     => { ensure => [ 'present' ] };
+      allow     => { ensure => [ 'present' ] },
+      weak      => true;
   }
 
   constraint {
     "python-pip-package":
       resource  => Package['python-pip'],
-      allow     => { ensure => [ 'present' ] };
+      allow     => { ensure => [ 'present' ] },
+      weak      => true;
   }
 }
